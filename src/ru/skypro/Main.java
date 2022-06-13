ea43b4b6afa17b0f6c41976dde71b971398a73f3
Scherbi <shherbakov99ilya@mail.ru>
2022-06-13 13:54:04 +0300

    ДЗ4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Задание 1");
+        int i=1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        //i=10;
+        System.out.println(" ");
+        for (i=10; i > 0; i--) {
+            System.out.print(i+" ");
+        }
+
+        System.out.println("Задание 2");
+        int day=5;
+
+        for (i = 1; i <= 31; ) {
+
+            System.out.println("Сегодня пятница - " + day + "e число");
+            day=day+7;
+            i=i+7;
+            if (day>31) {
+                break;
+            }
+        }
+
+        System.out.println("Задание 3");
+        for (i = 0; i<2400; i++) {
+            int a=1822;
+            int b=2122;
+            if (i >= a && i <= b && i % 79 == 0) {
+                System.out.println("В " + i + " комета пролетит над землей");
+            }
+        }
     }
 }
